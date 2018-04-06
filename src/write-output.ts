cc949748837a8ce33dbf36fcb6bbeabec9651933
Elliott Marquez <emarquez@google.com>
2018-04-05 22:16:24 -0700

    add license and fix format script

--- src/write-output.ts
+++ src/write-output.ts
@@ -1,3 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2018 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
 import * as dom5 from 'dom5'
 import * as fs from 'fs';
 import * as parse5 from 'parse5'
