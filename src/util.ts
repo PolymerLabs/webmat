e6a04f313f59543772158e831dc4a1f24e8d66c6
Elliott Marquez <emarquez@google.com>
2018-04-05 20:45:49 -0700

    documented util

--- src/util.ts
+++ src/util.ts
@@ -1,12 +1,27 @@
 import * as stream from 'stream';
 
+/**
+ * Caches the output of a stream.Readable in a Promise string.
+ */
 export class ReadableStreamCache {
+  /**
+   * Promise that resolves with the contents of the given stream
+   */
   readonly streamCached: Promise<string>;
 
+  /**
+   * @param stream Stream to be cached.
+   */
   constructor(stream: stream.Readable) {
     this.streamCached = this.readStream(stream);
   }
 
+  /**
+   * Reads a stream and returns a promise that resolves with its contents when
+   * stream ends.
+   *
+   * @param stream Stream to be cached
+   */
   private readStream(stream: stream.Readable): Promise<string> {
     let resolveStreamCached: (value: string) => void;
     const streamCached: Promise<string> =
